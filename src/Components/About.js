e791a18b1e679f277eed5bfe0cfa616f1c8d8259
Leonard-124 <leonardoduor91@gmail.com>
2025-04-20 21:04:15 +0300

    second commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <img src="./images1/Leonard.jpg" alt="Leonard" height={200} width={200}></img>
        <p>Hi, I am Leonard Spencer a dedicated web developer(Specializing in frontend majorly). Am passionate about coding and creating user friendly websites. My goal is to use technology to solve everyday problems.</p>
        <p>I am self-taught, and still continuing to learn more everyday.Through my learning experience I have gained skills in frameworks like HTML, CSS, Bootstrap,Tailwind,JavaScrypt, Nodejs, React, Figma among others.<br/>Through my experience I believe that I can deliver the best to any of my clients. Feel free to reach out.</p>
-       <img src="./images2/robot.jpg" alt="Book" height="150px" width="200px"></img>
+       <img src="./images1/robot.jpg" alt="Book" height="150px" width="200px"></img>
        <p>It's all about Tech.</p>
         </div>
         </div>
